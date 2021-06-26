d4c8d4b691476ef0fbc75be4f261535176b17cdb
2hangy <unravel_a@outlook.com>
2021-06-26 13:58:19 +0800

    添加serial结构，用于组合多个Structure

--- packet.py
+++ packet.py
@@ -1,35 +1,78 @@
 #!/usr/bin/python2
 # coding=utf-8
 
-# todo: 变长数据描述
-
 import ctypes
 from collections import OrderedDict
+from functools import partial
+
+def raw(v):
+    return ctypes.string_at(ctypes.addressof(v), ctypes.sizeof(v))
+
+def rawhex(v):
+    return raw(v).encode('hex')
 
 class StructWithDefault(ctypes.Structure):
     fields_desc = []
+
     def __init__(self, *args, **kwargs):
         for k,v in self.__class__.defaults.items()[len(args):]:
             if k not in kwargs:
                 kwargs[k] = v
         return ctypes.Structure.__init__(self, *args, **kwargs)
+
     def __new__(cls, *args, **kwargs):
-        cls._fields_ = [ (field[0], field[1][0]) for field in cls.fields_desc ] 
-        cls.defaults = OrderedDict([ (field[0], field[1][1]) for field in cls.fields_desc ])
-        print(cls.__name__)
+        if not hasattr(cls, '_fields_') or not cls._fields_:
+            cls._fields_ = [ (field[0], field[1][0]) for field in cls.fields_desc ] 
+        if not hasattr(cls, 'defaults') or not cls.defaults:
+            cls.defaults = OrderedDict([ (field[0], field[1][1]) for field in cls.fields_desc ])
         return ctypes.Structure.__new__(cls, *args, **kwargs)
 
-class struct_1(StructWithDefault):
-    _pack_ = 4
-    fields_desc = [
-        ('test', (ctypes.c_uint32, 0x11332244)),
-        ('c2', (ctypes.c_uint16, 0x5566)),
-        ('c1', (ctypes.c_uint8,  0x77)),
-        ('c3', (ctypes.c_uint16, 0x8899)),
-    ]
+class StructSerial(object):
+    def __init__(self, fields):
+        object.__setattr__(self, 'attr', OrderedDict())
+        for k, v in fields:
+            self.attr[k] = v()
+    def __getattr__(self, name):
+        if name in object.__getattribute__(self, 'attr'):
+            return self.attr[name]
+        else:
+            return None
+    def __setattr__(self, key, value):
+        if key in self.attr and \
+           type(value) == type(self.attr[key]):
+            self.attr[key] = value
+    def __iter__(self):
+        return iter(self.attr.copy())
+    def raw(self):
+        result = ''
+        for k in self:
+            v = getattr(self, k)
+            result += ctypes.string_at(ctypes.addressof(v), ctypes.sizeof(v))
+        return result
 
-a = struct_1(0x11220044, c1=0xcc)
+# example
 
-print(hex(a.test))
-stream = ctypes.string_at(ctypes.addressof(a), ctypes.sizeof(a))
-print(stream.encode('hex'))
+def test():
+    class struct_1(StructWithDefault):
+        _pack_ = 4
+        fields_desc = [
+            ('test', (ctypes.c_uint32, 0x11332244)),
+            ('c2', (ctypes.c_uint16, 0x5566)),
+            ('c1', (ctypes.c_uint8,  0x77)),
+            ('c3', (ctypes.c_uint16, 0x8899)),
+        ]
+    
+    s = StructSerial([
+            ('f1', struct_1),
+            ('f2', partial(struct_1, 0x66aaaa55, c3=0xbbdd)),
+        ])
+    s.f1.test = 0x99887766
+    print(hex(s.f1.test))
+    print(raw(s.f2).encode('hex'))
+    print(s.raw().encode('hex'))
+    a = struct_1(0x11220044, c1=0xcc)
+    print(hex(a.test))
+    print(raw(a).encode('hex'))
+
+if __name__ == '__main__':
+    test()
